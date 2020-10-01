2a920d6071939ca546b3ce4f70287350ef89084a
Shiro Retro <65318482+github-retro@users.noreply.github.com>
2020-10-01 13:24:56 +0930

    Change to how data formed
    Changed so BASIC data exported matches TILE_BASE data expected by VERA. Allows for x-y flips and palette offset.

--- Tiled-Tools/tiled-basic-map-extension.js
+++ Tiled-Tools/tiled-basic-map-extension.js
@@ -2,6 +2,9 @@
 // The map is saved in a BASIC data format.
 // Insired by Johan Kårlin
 // https://github.com/github-retro/X16-tools/tree/master/Tiled-Tools
+// 
+// reference
+// https://github.com/commanderx16/x16-docs
 
 var basicDataMapFormat = {
     name: "X16 BASIC Map",
@@ -10,6 +13,8 @@ var basicDataMapFormat = {
     write: function(map, fileName) {
 		// Ask user for BASIC line start
 		var basicLineStart = parseInt(tiled.prompt("BASIC Line start:", "10000"));
+		// Ask user for palette offset
+		var paletteOffset = parseInt(tiled.prompt("Palette Offset:", "0"));
 		
 		// Create file and write width and height
         var file = new TextFile(fileName, TextFile.WriteOnly);
@@ -20,7 +25,7 @@ var basicDataMapFormat = {
 		file.writeLine(basicLineStart + " REM MAP TILE WIDTH AND HEIGHT");
 		basicLineStart++;
 		file.writeLine(basicLineStart + " DATA " + map.tileWidth + ',' + map.tileHeight);
-		file.writeLine(basicLineStart + " REM MAP DATA IN 16 BYTE SEGMENTS");
+		file.writeLine(basicLineStart + " REM MAP DATA IN 8 x 2 BYTE SEGMENTS - VERA TILE_BASE DATA");
 		basicLineStart++;
 		
 		// Write each tile layer in map
@@ -31,23 +36,34 @@ var basicDataMapFormat = {
                 // Write each row in current layer
 				for (y = 0; y < layer.height; ++y) {
                     
-					// Write each column in current layer, create BASIC data lines with 16 elements in each
-					for (x = 0; x < layer.width; x=x+16) {
-
-						segment = " DATA ";
-						for (s = 0; s < 16; ++s){
+					// Write each column in current layer, create BASIC data lines with 8 elements in each
+					// each element is 2 bytes
+					// https://github.com/commanderx16/x16-docs/blob/master/VERA%20Programmer's%20Reference.md#tile-mode-248-bpp
+					// 
+					for (x = 0; x < layer.width; x=x+8) {
+						// reset the linebuffer 
+						linebuffer = " DATA ";
+						for (s = 0; s < 8; ++s){
 							var cell = layer.cellAt(x+s, y);
-							val = cell.tileId;
-							if (val == "-1"){
-								val = "0";
+							// Build and write 16 bit word with information about palette, flipping and 10 bit tile id
+							var word = (paletteOffset << 12) + (cell.tileId & 1023);
+							if (cell.flippedVertically) {
+								word += 0x0800; 
 							}
-							segment = segment + val.toString(10) + ",";
+							if (cell.flippedHorizontally) {
+								word += 0x0400;
+							}
+							TileBase0 = ((word >> 8) & 0xff);
+							TileBase1 = ((word >> 0) & 0xff);
+							
+							linebuffer = linebuffer + TileBase0.toString(10) + "," + TileBase1.toString(10) + "," ;
+							
  						}
 						// remove the last , from the string 
-						segment = segment.substring(0,segment.length - 1);
+						linebuffer = linebuffer.substring(0,linebuffer.length - 1);
 						// increase the BASIC line number and write out the data
 						++basicLineStart
-						file.writeLine(basicLineStart + segment);	
+						file.writeLine(basicLineStart + linebuffer);	
                     }
                 }
             }
